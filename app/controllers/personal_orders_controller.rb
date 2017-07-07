6f8c63ab8fde6d26990e45579667c62558db21b2
Kevin Yan <kevin@Kevins-MacBook-Pro.local>
2017-07-07 14:25:41 -0400

    update personal_orders controller to include the parent order_id

--- app/controllers/personal_orders_controller.rb
+++ app/controllers/personal_orders_controller.rb
@@ -1,18 +1,29 @@
 class PersonalOrdersController < ApplicationController
 
+  def index
+  end
+
   def new
     @personal_order = PersonalOrder.new
   end
 
   def create
     @personal_order = PersonalOrder.new(personal_order_params)
+    # add the referenced object id as well
+    @personal_order.order_id = params[:order_id]
+    # params_with_order = .merge(:order_id => @)
     if @personal_order.save
       flash[:notice] = 'saved!'
       redirect_to orders_path
     else
-      flash[:notice] = 'error!'
-      render('new');
+      flash[:notice] = @personal_order.errors.full_messages;
+      render :new
     end
+  end
+
+  def show
+    @personal_order = PersonalOrder.new
+    @order_ref = params[:id]
   end
 
   private
