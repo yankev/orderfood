42892e16678618c750df5b92ac13fe517f96f2e8
Kevin Yan <kevin@Kevins-MacBook-Pro.local>
2017-07-08 18:13:50 -0400

    delete all children on removal

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
-  has_many :orders
-  has_many :personal_orders
+  has_many :orders, :dependent => :destroy
+  has_many :personal_orders, :dependent => :destroy
 end
