985056ddcbb49e3a02adb8d7d5743a0968fb9d16
Kevin Yan <kevin@Kevins-MacBook-Pro.local>
2017-07-09 00:08:58 -0400

    handle user info editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
 
   def index
+    redirect_to signup_path
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(required_user_params)
+      redirect_to orders_path
+    else
+      render :edit
+    end
   end
 
   def new
