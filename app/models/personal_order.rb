88a3c2795d3aa225d874c8f23cc56d0ac7080416
Kevin Yan <kevin@Kevins-MacBook-Pro.local>
2017-07-09 02:19:43 -0400

    update models/validations

--- app/models/personal_order.rb
+++ app/models/personal_order.rb
@@ -1,7 +1,6 @@
 class PersonalOrder < ApplicationRecord
   belongs_to :order
   validates :price, :presence => true, numericality: true
-  validates :name, :presence => true
   validates :items, :presence => true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   has_many :orders, :dependent => :destroy
   has_many :personal_orders, :dependent => :destroy
 end
